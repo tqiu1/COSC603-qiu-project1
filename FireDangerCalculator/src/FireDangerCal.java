f278ea993aff549688a823283a5a6e9a7c19c12f
tqiu1 <tqiu1@students.towson.edu>
2016-03-15 21:31:27 -0400

    Re-coded program is ready

--- FireDangerCalculator/src/FireDangerCal.java
+++ FireDangerCalculator/src/FireDangerCal.java
@@ -1,4 +1,5 @@
 import java.lang.Math;  
+import java.util.Scanner;
 /**
 *ROUTINE FOR COMPUTING NATIONAL FIRE DANGER RATINGS AND FIRE LOAD INDEX DATA NEEDED FOR 
 *THE CALCULATIONS ARE: 
@@ -38,19 +39,23 @@ public class FireDangerCal {
 	double[] C=new double[5];
 	double[] D=new double[7];
 	
-	double dry;
-    double wet;
-    double isnow;
-    int precip;
-    double wind;
-    double buo;
-    int iherb;
-    int df;
-    double ffm;
-    double adfm;
-    double grass;
-    double timber;
-    double fload;
+	static double dry;
+	static double wet;
+	static double isnow;
+	static int precip;
+	static double wind;
+	static double buo;
+	static int iherb;
+	static int df;
+	static double ffm;
+	static double adfm;
+	static double grass;
+	static double timber;
+	static double fload;
+    
+    FireDangerCal(){
+    	
+    }
 	
 /**	
  *Analyze the Danger Rate
@@ -84,36 +89,36 @@ public void getDanger(double dry,
 			cal_DryFactor();
 			
 			// adjust fine fuel moist		
-			cal_AdjustedFuelMoist();
+			adjust_FineFuelMoisture();
 		}
 	
 	if (precip>0.1){  // is it rain?
 		//There is rain (precipitation >0.1 inch), we must reduce the build up index (BUO) by 
 		//an amount equal to rain fall
-		BUO=-50*Math.log(1-(1-Math.exp(-BUO/50))*Math.exp(-1.175*(precip-0.1)));
+		adjust_BUI();
+	}else{
+		//After correct for rain, if any, we are ready to add today's dring factor to obtain the 
+		//current build up index
+		increase_BUIBYDryingFactor();
 		
-	}
+		//we will adjust the grass index for heavy fuel lags. The result will be the timber spread index
+		//The adjusted fuel moisture, ADFM, Adjusted for heavy fuels, will now be computed.
+		cal_AdjustedFuelMoist();
+		
+		if (ADFM>33){
+			set_AllSpreadIndexToOne();
+
+			}else{
+				//whether wind>14? and calculate grass and timber spread index
+				cal_GrassAndTimber();
+				//Both BUI and Timber spread index are not 0?
+				if (!((TIMBER==0)&(BUO==0))){
+					cal_FireLoadRating();
+				}
+			}
+		}
+}
 	
-}
-
-
-
-/**	
-*Calculate Spread Index and Fire Load
-*/  
-public void cal_SpreadIndexAndFireLoad(){
-	
-}
-
-
-
-/**	
-*Calculate Grass and Timber Indexes
-*/  
-public void cal_GrassAndTimberInexes(){
-
-}
-
 
 
 /**	
@@ -137,11 +142,8 @@ public void get_InputValues(){
 	D[4]=5.0;
 	D[5]=4.0;
 	D[6]=3.0;
-		
 	
 }
-
-
 
 /**	
 *Set Initial Table Values
@@ -155,15 +157,44 @@ public void get_TableValues(){
 }
 
 
-
 /**	
-*Output the updated parameters
+*get user inputs for some parameters
+*/  
+public void get_UserInput(){
+	  Scanner s = new Scanner(System.in);
+	  // get User inputs 
+	  System.out.println("Please enter a number for Dry (double):");
+	  DRY = s.nextDouble();
+	  System.out.println("Please enter a number for Wet (double):");
+	  WET = s.nextDouble(); 
+	  System.out.println("Please enter a number for ISNOW (double):");
+	  ISNOW = s.nextDouble();
+	  System.out.println("Please enter a number for Wind(double):");
+	  WIND = s.nextDouble(); 
+	  System.out.println("Please enter a number for BUO (double):");
+	  BUO = s.nextDouble();
+	  System.out.println("Please enter a number for Iherb (int):");
+	  IHERB = s.nextInt(); 
+	  System.out.println("****Data for parameters are collected****.");
+	  System.out.println("Data for parameters are collected.");
+	  dry=DRY;
+	  wet=WET;
+}
+/**	
+*Output the finally updated parameters
 */  
 public void output_UpdatedData(){
-	
+
+	  System.out.println("\nParameters are updated as following...");
+	  
+	  System.out.println("Dry (double): "+DRY);
+	  
+	  System.out.println("Wet (double): "+WET);
+	  System.out.println("ISNOW (double): "+ISNOW);
+	  System.out.println("Wind(double): "+WIND);
+	  System.out.println("BUO (double): "+BUO);
+	  System.out.println("Iherb (int): "+IHERB);
 }
-
-
 
 /**	
 *Set All Spread Index To Zero
@@ -172,16 +203,6 @@ public void set_AllSpreadIndexToZero(){
 	GRASS=0;   
 	TIMBER=0;
 }
-
-
-
-/**	
-*Set All Spread Index To One
-*/  
-public void set_AllSpreadIndexToOne(){
-
-}
-
 
 
 /**	
@@ -207,7 +228,6 @@ public void cal_FineFuelMoisture(){
 }
 
 
-
 /**	
 *Calculate Dry Factor
 */  
@@ -225,36 +245,6 @@ public void cal_DryFactor(){
 *Adjust Fine Fuel Moisture
 */  
 public void adjust_FineFuelMoisture(){
-	
-}
-
-
-
-/**	
-*Adjuest BUI
-*/  
-public void adjust_BUI(){
-	if (precip>0){  // is it rain?
-		//Adjust the BUO
-		BUO =50*(Math.log(1-(1-Math.exp(-buo/50))*Math.exp(-1.175*(precip-0.1))));
-	}
-}
-
-
-
-/**	
-*Increase BUI By Drying Factor
-*/  
-public void increase_BUIBYDryingFactor(){
-	
-}
-
-
-
-/**	
-*Calculate Adjusted Fire Moisture
-*/  
-public void cal_AdjustedFuelMoist(){
 	// If FFM is one or less we set it to one		
 	if ((FFM-1)<=0){
 		FFM=1;
@@ -265,35 +255,89 @@ public void cal_AdjustedFuelMoist(){
 }
 
 
+/**	
+*Adjuest BUI
+*/  
+public void adjust_BUI(){
+	BUO=-50*Math.log(1-(1-Math.exp(-BUO/50))*Math.exp(-1.175*(precip-0.1)));
+		if (BUO<0){
+			BUO=0;
+		}
+}
+
+
+/**	
+*Increase BUI By Drying Factor
+*/  
+public void increase_BUIBYDryingFactor(){
+	BUO=BUO+DF;
+}
+
+/**	
+*Calculate Adjusted Fire Moisture
+*/  
+public void cal_AdjustedFuelMoist(){
+	ADFM=0.9*FFM+0.5+9.5*Math.exp(-BUO/50);
+}
+
+/**	
+*Set All Spread Index To One
+*/  
+public void set_AllSpreadIndexToOne(){
+	//test to see if the fuel moistures are greater than 33 percent.
+	//if they are, set their index value to 1
+	if ((FFM>33)){  // fine fuel greater than 33?
+		GRASS=0;   
+		TIMBER=0;
+	}else{
+		TIMBER=1;
+	}
+}
+
 
 /**	
 *Calculate Grass Index
 */  
-public void cal_Grass(){
-	
-}
+public void cal_GrassAndTimber(){
+	if (wind>=14){
+		GRASS=0.00918*(wind+14)*Math.pow((33-FFM),1.65)-3;
+	}else{
+		TIMBER=GRASS=0.01312*(wind+6)*Math.pow((33-ADFM),1.65)-3;
+		GRASS=0.01312*(wind+6)*Math.pow((33-FFM),1.65)-3;
+		if (TIMBER<=1){TIMBER=1;}
+		if (GRASS<1){GRASS=1;}
 
+	}
+
+}
 
 
 /**	
 *Calculate Fire and Load Rating
 */  
 public void cal_FireLoadRating(){
-	
+	FLOAD=1.75*Math.log10(TIMBER)+0.32*Math.log10(BUO)-1.640;
+	//ensure that FLOAD is greater than 0, otherwise set it to 0;
+	if (FLOAD<0){FLOAD=0;
+		}else{FLOAD=Math.pow(10, FLOAD);
+	}
 }
-
 
 
 /**	
 *Main function that call test class
 */  
 public static void main(java.lang.String[] args){
+	FireDangerCal test=new FireDangerCal();
+	test.get_InputValues();
+	test.get_TableValues();
+	test.get_UserInput();
+	test.getDanger(dry, wet,isnow,precip,wind,buo,iherb,df,ffm,adfm,grass,timber,fload);
+	test.output_UpdatedData();
 	
 }
 
 
-
-	
 	
 	
 }
